2e6d8c0a08283e24261232371320b1183d09388b
leonardo-schneider2 <leocoelho256@gmail.com>
2022-05-25 09:17:56 -0300

    O papai acabou! u.u

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -18,19 +18,60 @@ const getAnimalperRegion = (region) =>
   (species.filter((specie) =>
     (specie.location === region)).map((specie) => specie.name));
 
+// Função que filtra por sexo e organiza alfabeticamente
+
+const sortedSex = (region, sex) =>
+  (getAnimalperRegion(region).map((animal) =>
+    ({ [animal]: species.find((specie) =>
+      specie.name === animal).residents.filter((resident) =>
+      resident.sex === sex).map((objResident) => objResident.name).sort(),
+    })));
+
+// Função que somente filtra por sexo sem organizar
+
+const sexNoSort = (region, sex) =>
+  (getAnimalperRegion(region).map((animal) =>
+    ({ [animal]: species.find((specie) =>
+      specie.name === animal).residents.filter((resident) =>
+      resident.sex === sex).map((objResident) => objResident.name),
+    })));
+
+// Função que somente organiza sem filtrar
+
+const sortNoSex = (region, sex) =>
+  (getAnimalperRegion(region).map((animal) =>
+    ({ [animal]: species.find((specie) =>
+      specie.name === animal).residents.map((objResident) =>
+      objResident.name).sort(),
+    })));
+
+// Função que somente organiza sem filtrar
+
+const noSortNoSex = (region, sex) =>
+  (getAnimalperRegion(region).map((animal) =>
+    ({ [animal]: species.find((specie) =>
+      specie.name === animal).residents.map((objResident) =>
+      objResident.name),
+    })));
+
+// Função pra dar resultado verificando sex e sorted
+
 const getAnimalAndNames = (location, sex, sorted) => {
   if (sex && sorted) {
     return location.reduce((acc, region) =>
-      ({ ...acc, [region]: getAnimalperRegion(region) }), {});
+      ({ ...acc, [region]: sortedSex(region, sex) }), {});
   } // else
   if (sex) {
-    return false;
+    return location.reduce((acc, region) =>
+      ({ ...acc, [region]: sexNoSort(region, sex) }), {});
   } // else
   if (sorted) {
-    return false;
+    return location.reduce((acc, region) =>
+      ({ ...acc, [region]: sortNoSex(region, sex) }), {});
   }
   // else
-  return false;
+  return location.reduce((acc, region) =>
+      ({ ...acc, [region]: noSortNoSex(region, sex) }), {});
 };
 
 // Função Raiz da Questão
@@ -48,6 +89,6 @@ function getAnimalMap(options) {
   return getAnimalAndNames(location, sex, sorted);
 }
 
-console.log(getAnimalMap({ sex: 'female', includeNames: true, sorted: true }));
+console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
